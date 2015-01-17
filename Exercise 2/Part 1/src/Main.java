e9a06812a632acaf102b1c96664cf8b043d69be2
ChrisLane <ChrisLane@users.noreply.github.com>
2015-01-17 18:49:32 +0000

    Add a simple solution for exercise 2 part 1
    Adds "Hello, World!" text
    Adds movement changing on button press

--- Exercise 2/Part 1/src/Main.java
+++ Exercise 2/Part 1/src/Main.java
@@ -1,6 +1,46 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.util.Delay;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// Print "Hello, World!" to the robot display for 5ms
+		LCD.drawString("Hello, World!", 0, 0);
+		Delay.msDelay(5000);
+
+
+		// Robot sizes
+		double wheelDiameter = 10;
+		double trackWidth = 5;
+
+		// On a button press, change the robot's movements
+		Button.waitForAnyPress();
+		DifferentialPilot pilot = new DifferentialPilot(wheelDiameter, trackWidth, Motor.A, Motor.B);
+		pilot.setTravelSpeed(30);
+
+		while (!Button.ESCAPE.isDown()) {
+			while (!Button.ENTER.isDown()) {
+				pilot.travel(50, true);
+				pilot.rotate(90, true);
+			}
+
+			while (!Button.ENTER.isDown()) {
+				pilot.travel(50, true);
+				pilot.rotate(-90, true);
+			}
+
+			while (!Button.ENTER.isDown()) {
+				pilot.travel(-50, true);
+				pilot.rotate(90, true);
+			}
+
+			while (!Button.ENTER.isDown()) {
+				pilot.travel(-50, true);
+				pilot.rotate(-90, true);
+			}
+		}
 	}
 }
