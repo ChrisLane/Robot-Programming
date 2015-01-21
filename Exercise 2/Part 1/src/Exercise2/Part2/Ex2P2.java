28bbf327ed2df8e9e132fb1a5a0c5d640f9a12f4
frebib <frebib@gmail.com>
2015-01-21 09:54:01 +0000

    Added a simple (really, this took the whole of 10 minutes) solution for Part2

---
+++ Exercise 2/Part 1/src/Exercise2/Part2/Ex2P2.java
@@ -0,0 +1,33 @@
+package Exercise2.Part2;
+
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.robotics.navigation.DifferentialPilot;
+import Exercise2.JeffRobot;
+
+public class Ex2P2 {
+	
+	public static void main(String[] args) {
+		TouchSensor ts = new TouchSensor(SensorPort.S4);
+		DifferentialPilot pilot = JeffRobot.getDifferentialPilot();
+		
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+			@Override
+			public void buttonReleased(Button b) {}
+			@Override
+			public void buttonPressed(Button b) {
+				System.exit(0);
+			}
+		});
+		
+		pilot.forward();
+		while (true) {
+			while (!ts.isPressed()) {}
+			pilot.travel(-20);
+			pilot.rotate(90);
+			pilot.forward();
+		}
+	}
+}
