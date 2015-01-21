c31e9a573f37045a2a74af354c46078bb3e468b6
frebib <frebib@gmail.com>
2015-01-21 08:15:21 +0000

    Added a circle path

---
+++ Exercise 2/Part 1/src/Exercise2/Part1/CirclePath.java
@@ -0,0 +1,15 @@
+package Exercise2.Part1;
+
+import lejos.robotics.navigation.DifferentialPilot;
+
+public class CirclePath extends MovementPath {
+	
+	public CirclePath(DifferentialPilot pilot) {
+		super(pilot, "Circle");
+	}
+	
+	@Override
+	protected void path() {
+		pilot.arc(40, 90);
+	}
+}
