76e1dc6d9db9742dca22dac32ed8134aad48c84c
frebib <frebib@gmail.com>
2015-01-26 16:16:56 +0000

    Renameing to GeoffBot

--- src/GeoffBot.java
+++ src/GeoffBot.java
@@ -1,14 +1,16 @@
 import lejos.nxt.Motor;
+import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.RConsole;
 import lejos.robotics.navigation.DifferentialPilot;
 
-public class JeffRobot {
+public class GeoffBot {
 	public final static double WHEELDIAMETER = 7, TRACKWIDTH = 12.5; // Both in cm
 
 	private static DifferentialPilot diffPilot;
 
 	static {
 		diffPilot = new DifferentialPilot(WHEELDIAMETER, TRACKWIDTH, Motor.B, Motor.C);
+		Bluetooth.setFriendlyName("GeoffBot");
 	}
 
 	public static DifferentialPilot getDifferentialPilot() {
