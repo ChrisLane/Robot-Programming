40b4aa79f07c6ccfa10d5964611dd5454e367c11
frebib <frebib@gmail.com>
2015-01-27 02:35:55 +0000

    Updated Ex2P1 main to work with new MovementPath structure.
    Signed-off-by: frebib <frebib@gmail.com>

--- src/Ex2P1.java
+++ src/Ex2P1.java
@@ -11,13 +11,13 @@ import lejos.robotics.navigation.DifferentialPilot;
 
 public class Ex2P1 {
 	public static void main(String[] args) {
-		// JeffRobot.connectRemote();
-		
+		GeoffBot.connectRemote();
+
 		LCD.clear();
 		LCD.drawString("   Hello World\n\n\n  Press Enter to\n    continue...", 0, 2);
 
 		DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
-		pilot.setTravelSpeed(35);
+		pilot.setTravelSpeed(20);
 
 		MovementPath[] paths = {
 				new SquarePath(pilot), new Figure8Path(pilot), new BackForthPath(pilot), new CirclePath(pilot)
@@ -29,22 +29,45 @@ public class Ex2P1 {
 
 			@Override
 			public void buttonPressed(Button b) {
-				LCD.clear();
+				// Don't fire on first
+				if (index < 0) {
+					index++;
+					return;
+				}
+
 				LCD.drawString("Stopping...", 3, 5);
-				path.stop();
+
+				// Stop the current path if it is running and wait for termination
+				if (path.isRunning())
+					path.waitStop();
+
+				// Loop path index
 				if (++index == paths.length)
 					index = 0;
 				path = paths[index];
-				LCD.clear();
 			}
 
 			@Override
 			public void buttonReleased(Button b) {
+				// Draw path name in middle of screen
+				LCD.clear();
 				LCD.drawString(path.getName(), (int) Math.ceil((LCD.DISPLAY_CHAR_WIDTH - path.getName().length()) / 2.0), 2);
 				path.start();
 			}
 		});
 
-		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		// This doesn't work?!
+		// Exit program when Escape button is pressed
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+			@Override
+			public void buttonPressed(Button b) {
+				System.exit(1);
+			}
+			@Override
+			public void buttonReleased(Button b) {
+			}
+		});
+
+		while (!Button.ESCAPE.isDown());
 	}
 }
