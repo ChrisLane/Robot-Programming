32d92be22032d35ab844a850a038b18f47f35e87
ChrisLane <ChrisLane@users.noreply.github.com>
2015-02-10 14:47:46 +0000

    Let's get started on P2

---
+++ src/rp/Ex3/Part2/Ex3P2.java
@@ -0,0 +1,16 @@
+package rp.Ex3.Part2;
+
+import rp.RunUtil;
+
+public class Ex3P2 extends RunUtil{
+
+	@Override
+	public void run() {
+
+	}
+
+	public static void main(String[] args) {
+		Ex3P2 program = new Ex3P2();
+		program.run();
+	}
+}
