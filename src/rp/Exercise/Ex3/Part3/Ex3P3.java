987d8283bbcbac4859e32b0c3339f64bc0ccc805
ChrisLane <ChrisLane@users.noreply.github.com>
2015-02-10 16:32:21 +0000

    Cleanup Ex3P3 code formatting

--- src/rp/Exercise/Ex3/Part3/Ex3P3.java
+++ src/rp/Exercise/Ex3/Part3/Ex3P3.java
@@ -14,38 +14,38 @@ public class Ex3P3 extends RunSystem{
 	private double leftT = 118; 		//right and left thresholds before turning either side
 	private double rightT = 50;
 	private double radius;
+
+	@Override
 	public void run(){
 		DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
 		NXTCam cam = new NXTCam(GeoffBot.getCameraPort());
 		cam.setTrackingMode(NXTCam.COLOR);
 		cam.sortBy(NXTCam.SIZE);
 		cam.enableTracking(true);
-		
+
 		while (isRunning){
 			Rectangle2D rec = cam.getRectangle(0);
 			pilot.forward();
-			if(rec.getX() < rightT){
+			if (rec.getX() < rightT) {
 				radius = 0.7 * (rightT - rec.getX());
 				pilot.arcForward(-radius);
 				Delay.msDelay(300);
 			}
-			else if(rec.getX() > leftT){
+			else if (rec.getX() > leftT) {
 				radius = 0.7 * (rec.getX() - leftT);
 				pilot.arcForward(radius);
 				Delay.msDelay(300);
 			}
 			else
 				pilot.forward();
-				System.out.println(rec.getWidth());
+			System.out.println(rec.getWidth());
 			//System.out.println(numberOfObjects);
 			//Delay.msDelay(300);
-			
 		}
 	}
 
 	public static void main(String[] args) {
 		Ex3P3 program = new Ex3P3();
 		program.run();
-
 	}
 }
