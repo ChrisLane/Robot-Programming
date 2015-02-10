4c0d8eada17468b9a6674688d8748ab8be53d8bb
ChrisLane <ChrisLane@users.noreply.github.com>
2015-02-10 13:59:11 +0000

    Change calibrate class to use RunUtil and print
    Now prints values of calibrations that we can move into GeoffBot

--- src/rp/Ex3/Utils/CalibrateLightSensors.java
+++ src/rp/Ex3/Utils/CalibrateLightSensors.java
@@ -1,39 +1,48 @@
 package rp.Ex3.Utils;
 
 import rp.GeoffBot;
+import rp.RunUtil;
 
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.LightSensor;
 import lejos.util.Delay;
 
-public class CalibrateLightSensors {
+public class CalibrateLightSensors extends RunUtil{
 
 	public void run() {
 		LightSensor lsLeft = new LightSensor(GeoffBot.getLightSensorLeftPort(), true);
+		LightSensor lsRight = new LightSensor(GeoffBot.getLightSensorRightPort(), true);
 		LCD.drawString("Calibrate left\nsensor LOW...", 0, 0);
 		Button.waitForAnyPress();
 		lsLeft.calibrateLow();
+		LCD.drawString(Integer.toString(lsLeft.getLow()), 0, 5);
+		Button.waitForAnyPress();
+		LCD.clear();
+
+		LCD.drawString("Calibrate right\nsensor LOW...", 0, 0);
+		Button.waitForAnyPress();
+		lsRight.calibrateLow();
+		LCD.drawString(Integer.toString(lsRight.getLow()), 0, 5);
+		Button.waitForAnyPress();
 		LCD.clear();
 
 		LCD.drawString("Calibrate left\nsensor HIGH...", 0, 0);
 		Button.waitForAnyPress();
 		lsLeft.calibrateHigh();
-		LCD.clear();
-
-		LightSensor lsRight = new LightSensor(GeoffBot.getLightSensorRightPort(), true);
-		LCD.drawString("Calibrate right\nsensor LOW...", 0, 0);
+		LCD.drawString(Integer.toString(lsLeft.getHigh()), 0, 5);
 		Button.waitForAnyPress();
-		lsRight.calibrateLow();
 		LCD.clear();
 
 		LCD.drawString("Calibrate right\nsensor HIGH...", 0, 0);
 		Button.waitForAnyPress();
 		lsRight.calibrateHigh();
+		LCD.drawString(Integer.toString(lsRight.getHigh()), 0, 5);
+		Button.waitForAnyPress();
 		LCD.clear();
 
 
-		while (!Button.ESCAPE.isDown()) {
+		while (isRunning) {
 			LCD.clear();
 			LCD.drawString(lsLeft.getLightValue() + "     " + lsRight.getLightValue(), 0, 0);
 			Delay.msDelay(100);
