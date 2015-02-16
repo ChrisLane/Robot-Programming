d3383fca392a115882e3b1bf1126725ef139f7ab
frebib <frebib@gmail.com>
2015-02-16 16:50:33 +0000

    Prevented many calls of onIntersectionArrive when light levels change (untested)

--- src/rp/Sensor/IntersectionSensor.java
+++ src/rp/Sensor/IntersectionSensor.java
@@ -8,21 +8,28 @@ import rp.Listener.LineListener;
 public class IntersectionSensor {
 	private boolean leftDark, rightDark;
 	private ArrayList<IntersectionListener> listeners;
+	private boolean onIntersection;
 
 	public IntersectionSensor(BlackLineSensor left, BlackLineSensor right) {
 		this.listeners = new ArrayList<IntersectionListener>();
 		left.addChangeListener(new LineListener() {
 			@Override
 			public void lineChanged(boolean onLine, int lightValue) {
-				leftDark = onLine;
-				stateChanged();
+				if (onLine) {
+					System.out.println("left dark " + lightValue);
+					leftDark = onLine;
+					stateChanged();
+				}
 			}
 		});
 		right.addChangeListener(new LineListener() {
 			@Override
 			public void lineChanged(boolean onLine, int lightValue) {
-				rightDark = onLine;
-				stateChanged();
+				if (onLine) {
+					System.out.println("right dark " + lightValue);
+					rightDark = onLine;
+					stateChanged();
+				}
 			}
 		});
 	}
@@ -33,8 +40,13 @@ public class IntersectionSensor {
 	}
 
 	private void stateChanged() {
-		if (this.leftDark && this.rightDark)
+		if (this.leftDark && this.rightDark && !onIntersection) {
+			System.out.println("intersectionchanged");
+			onIntersection = true;
 			for (IntersectionListener ls : this.listeners)
 				ls.onIntersectionArrive();
+		}
+		else
+			onIntersection = false;
 	}
 }
