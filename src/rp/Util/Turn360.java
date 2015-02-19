309cbd0eda1f64a17c9d2c4e3f21f7098b6b86d7
ChrisLane <ChrisLane@users.noreply.github.com>
2015-02-19 17:00:49 +0000

    Use this to adjust robot size values in future.

---
+++ src/rp/Util/Turn360.java
@@ -0,0 +1,20 @@
+package rp.Util;
+
+import rp.GeoffBot;
+
+import lejos.robotics.navigation.DifferentialPilot;
+
+public class Turn360 extends RunSystem {
+
+	DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
+
+	@Override
+	public void run() {
+		pilot.rotate(360);
+	}
+
+	public static void main(String[] args) {
+		Turn360 program = new Turn360();
+		program.run();
+	}
+}
