c385712369fa6a4c5889af3ee50778cb13f4529f
ChrisLane <ChrisLane@users.noreply.github.com>
2015-02-25 20:43:05 +0000

    Working Ex3P1A. Tried this two hours ago but only now it decided to work!
    FUUUUU

--- src/rp/Exercise/Ex3/Part1/Ex3P1A.java
+++ src/rp/Exercise/Ex3/Part1/Ex3P1A.java
@@ -8,7 +8,6 @@ import lejos.robotics.RangeFinder;
 import lejos.robotics.navigation.DifferentialPilot;
 
 public class Ex3P1A extends RunSystem {
-	private double mv = 20; // manipulated variable (speed)
 
 	public static void main(String[] args) {
 		Ex3P1A program = new Ex3P1A();
@@ -19,15 +18,25 @@ public class Ex3P1A extends RunSystem {
 	public void run() {
 		DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
 		RangeFinder rf = new UltrasonicSensor(GeoffBot.getFrontUltrasonicPort());
-		pilot.forward();
-		while (isRunning) {
-			double pv = rf.getRange();
-			double sp = 25;
-			double threshold = 5 * sp;
-			double error = (pv <= threshold ? sp - pv : pv - sp);
 
-			mv += (0.003 * error);
-			pilot.setTravelSpeed(mv);
+		pilot.forward();
+
+		while (isRunning) {
+			double mv = 0;
+			double range = rf.getRange();
+			double targetRange = 30;
+
+			if (range >= targetRange) {
+				double error = range - targetRange;
+				mv += 0.5 * error;
+				pilot.forward();
+				pilot.setTravelSpeed(mv);
+			} else if (range <= targetRange) {
+				double error = targetRange - range;
+				mv += 2 * error;
+				pilot.backward();
+				pilot.setTravelSpeed(mv);
+			} else pilot.setTravelSpeed(0);
 		}
 	}
 }
