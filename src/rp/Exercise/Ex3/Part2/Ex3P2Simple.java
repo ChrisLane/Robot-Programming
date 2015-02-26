a8db9bf36191c2cf1374385955817b8b0233ff98
ChrisLane <ChrisLane@users.noreply.github.com>
2015-02-26 12:39:42 +0000

    Add comments for P2Simple

--- src/rp/Exercise/Ex3/Part2/Ex3P2Simple.java
+++ src/rp/Exercise/Ex3/Part2/Ex3P2Simple.java
@@ -11,7 +11,6 @@ import java.util.Queue;
 
 public class Ex3P2Simple extends RunSystem implements LineListener {
 	private final DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
-	private final int lightThreshold = 75;
 	private Queue<Node> path;
 	private Node location;
 	private Compass facing;
@@ -27,6 +26,7 @@ public class Ex3P2Simple extends RunSystem implements LineListener {
 		this.facing = facing;
 		this.location = location;
 
+		int lightThreshold = 75;
 		lsLeft = new BlackLineSensor(GeoffBot.getLightSensorLeftPort(), true, lightThreshold).addChangeListener(this);
 		lsRight = new BlackLineSensor(GeoffBot.getLightSensorRightPort(), true, lightThreshold).addChangeListener(this);
 
@@ -35,8 +35,7 @@ public class Ex3P2Simple extends RunSystem implements LineListener {
 	}
 
 	public static void main(String[] args) {
-		// GeoffBot.connectRemote();
-
+		//Path of nodes for robot to follow
 		Queue<Node> path = new Queue<>();
 		path.addElement(new Node(1, 0));
 		path.addElement(new Node(1, 1));
@@ -47,6 +46,7 @@ public class Ex3P2Simple extends RunSystem implements LineListener {
 		path.addElement(new Node(0, 1));
 		path.addElement(new Node(0, 0));
 
+		// Robot starts at node (0,0) and follows path above
 		Ex3P2Simple program = new Ex3P2Simple(path, new Node(0, 0), Compass.UP);
 		program.run();
 	}
@@ -55,6 +55,7 @@ public class Ex3P2Simple extends RunSystem implements LineListener {
 	public void run() {
 		while (!path.empty() && isRunning) {
 
+			// If both sensors are the line then we've reached an intersection. Rotate towards new target.
 			if (leftOnline && rightOnline) {
 				Node target = (Node) path.pop();
 
@@ -66,15 +67,20 @@ public class Ex3P2Simple extends RunSystem implements LineListener {
 				location = target;
 
 				pilot.rotate(heading.toDegrees());
-			} else if (leftOnline) {
+			}
+			// If the left sensor is on the line then turn left
+			else if (leftOnline) {
 				pilot.steer(180, -2, false);
-			} else if (rightOnline) {
+			}
+			// If the right sensor is on the line then turn right
+			else if (rightOnline) {
 				pilot.steer(180, 2, false);
 			}
 			pilot.forward();
 		}
 	}
 
+	/// Sets a boolean for if each sensor is on the line or not
 	public void lineChanged(BlackLineSensor sensor, boolean onLine, int lightValue) {
 		if (sensor == lsLeft)
 			leftOnline = onLine;
