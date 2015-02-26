afe5e71c7a794744d29c45bd223490451c244a2a
frebib <frebib@gmail.com>
2015-02-26 12:33:10 +0000

    Cleanups. Sorry Chris

--- src/rp/Exercise/Ex3/Part1/Ex3P1A.java
+++ src/rp/Exercise/Ex3/Part1/Ex3P1A.java
@@ -8,6 +8,10 @@ import lejos.robotics.RangeFinder;
 import lejos.robotics.navigation.DifferentialPilot;
 
 public class Ex3P1A extends RunSystem {
+	private final DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
+	private final RangeFinder rf = new UltrasonicSensor(GeoffBot.getFrontUltrasonicPort());
+
+	private final double targetRange = 30; // setpoint
 
 	public static void main(String[] args) {
 		Ex3P1A program = new Ex3P1A();
@@ -16,30 +20,27 @@ public class Ex3P1A extends RunSystem {
 
 	@Override
 	public void run() {
-		DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
-		RangeFinder rf = new UltrasonicSensor(GeoffBot.getFrontUltrasonicPort());
-
-		double mv = 0; // manipulated variable (travel speed)
 		while (isRunning) {
 			double range = rf.getRange(); // process variable
-			double targetRange = 30; // setpoint
+			double mv = 0; // manipulated variable (travel speed)
 
 			// If too far from target range, gradually increase forward speed
 			if (range >= targetRange) {
 				double error = range - targetRange;
 				mv += 0.5 * error;
-				pilot.forward();
 				pilot.setTravelSpeed(mv);
+				pilot.forward();
 			}
 			// If we're past the target range, gradually increase reverse speed
 			else if (range <= targetRange) {
 				double error = targetRange - range;
 				mv += 2 * error;
-				pilot.backward();
 				pilot.setTravelSpeed(mv);
+				pilot.backward();
 			}
 			// We're at the target distance, stop the robot.
-			else pilot.setTravelSpeed(0);
+			else
+				pilot.setTravelSpeed(0);
 		}
 	}
 }
