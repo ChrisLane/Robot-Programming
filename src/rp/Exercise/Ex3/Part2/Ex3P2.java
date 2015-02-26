34a64b08f95d7a84c9e22f71cb407f41b195f3f1
frebib <frebib@gmail.com>
2015-02-26 15:40:52 +0000

    Refactor Ex3P2 to use simple code as primary program

--- src/rp/Exercise/Ex3/Part2/Ex3P2.java
+++ src/rp/Exercise/Ex3/Part2/Ex3P2.java
@@ -1,142 +1,118 @@
 package rp.Exercise.Ex3.Part2;
 
 import rp.GeoffBot;
-import rp.Listener.IntersectionListener;
 import rp.Listener.LineListener;
 import rp.Sensor.BlackLineSensor;
-import rp.Sensor.IntersectionSensor;
 import rp.Util.RunSystem;
 
 import lejos.robotics.navigation.DifferentialPilot;
 
 import java.util.Queue;
 
-public class Ex3P2 extends RunSystem implements IntersectionListener, LineListener {
+public class Ex3P2 extends RunSystem implements LineListener {
 	private final DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
-	private BlackLineSensor lsLeft, lsRight;
-
 	private Queue<Node> path;
 	private Node location, target;
 	private Compass facing;
-	private Thread steerLeftThread, steerRightThread;
-
-	private boolean isTravelling = false;
+	private boolean leftOnline, rightOnline, onIntersection;
+	private BlackLineSensor lsLeft, lsRight;
 
 	public Ex3P2(Queue<Node> path, Node location, Compass facing) {
-		// If the path is empty then we can't do anything
 		if (path.empty())
 			return;
 
 		this.path = path;
 		this.facing = facing;
 		this.location = location;
+		this.leftOnline = true;
+		this.rightOnline = true;
 
-		lsLeft = new BlackLineSensor(GeoffBot.getLightSensorLeftPort(), true, 40).addChangeListener(this);
-		lsRight = new BlackLineSensor(GeoffBot.getLightSensorRightPort(), true, 40).addChangeListener(this);
+		int lightThreshold = 75;
+		lsLeft = new BlackLineSensor(GeoffBot.getLightSensorLeftPort(), true, lightThreshold).addChangeListener(this);
+		lsRight = new BlackLineSensor(GeoffBot.getLightSensorRightPort(), true, lightThreshold).addChangeListener(this);
 
 		GeoffBot.calibrateLeftLS(lsLeft);
 		GeoffBot.calibrateRightLS(lsRight);
 
-		// Detect intersections
-		new IntersectionSensor(lsLeft, lsRight, true).addChangeListener(this);
-
-		steerLeftThread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (isRunning) {
-					try {
-						wait(0);
-						Thread.sleep(250);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					if (isTravelling)
-						pilot.steer(-60);
-					else
-						pilot.forward();
-				}
-			}
-		});
-		steerRightThread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (isRunning) {
-					try {
-						wait(0);
-						Thread.sleep(250);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					if (isTravelling)
-						pilot.steer(60);
-					else
-						pilot.forward();
-				}
-			}
-		});
+		// Increased speed as robot can handle it fine
+		pilot.setTravelSpeed(25);
+		pilot.setRotateSpeed(180);
 	}
 
 	public static void main(String[] args) {
-
+		// Path of nodes for robot to follow
 		Queue<Node> path = new Queue<>();
 		path.addElement(new Node(1, 0));
 		path.addElement(new Node(1, 1));
-		path.addElement(new Node(0, 1));
-		path.addElement(new Node(0, 0));
-		path.addElement(new Node(1, 0));
+		path.addElement(new Node(1, 2));
+		path.addElement(new Node(1, 3));
+		path.addElement(new Node(1, 2));
 		path.addElement(new Node(1, 1));
+		path.addElement(new Node(1, 0));
+		path.addElement(new Node(0, 0));
 		path.addElement(new Node(0, 1));
 		path.addElement(new Node(0, 0));
 
+		// Robot starts at node (0,0) and follows path above
 		Ex3P2 program = new Ex3P2(path, new Node(0, 0), Compass.UP);
 		program.run();
 	}
 
 	@Override
-	public synchronized void run() {
-		while (!path.empty() && isRunning) {
-			target = (Node) path.pop();
+	public void run() {
+		intersectionHit(false);
+		while (isRunning) {
+			// If both sensors are the line then we've reached an intersection. Rotate towards new target.
+			if (leftOnline && rightOnline && !onIntersection) {
+				onIntersection = true;
+				// Finish path traversal if there are no more nodes left
+				if (path.empty())
+					return;
 
-			Compass heading = facing.getHeadingFrom(location.getCoord(), target.getCoord());
-			facing = facing.add(heading);
-
-			pilot.rotate(heading.toDegrees()); // Rotate to face target node if not already
-
-			// Drive to 'location'
-			isTravelling = true;
-			pilot.forward();
-
-			try { // the line and adjust angle while
-				this.wait(0); // Wait for intersection to be reached
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				intersectionHit(true);
 			}
+			else if (!leftOnline && !rightOnline)
+				// Added onIntersection to fix a big bug where it would pass all
+				// intersections in a straight line after the first one.
+				// This works by incrementing the current node after it has left the
+				// current intersection so not to skip them all at once.
+				onIntersection = false;
 
-			pilot.travel(4); // Travel to align wheels with the intersection
-			isTravelling = false;
-
-			location = target;  // We are now at the target location
+			// If on the line then turn the respective direction to correct
+			if (leftOnline)
+				pilot.arcForward(-40);
+			else if (rightOnline)
+				pilot.arcForward(40);
+			else
+				pilot.forward();
 		}
 	}
 
-	@Override
-	public synchronized void onIntersectionArrive() {
-		if (isTravelling)
-			notify(); // Wake up loop to continue on path
+	public void intersectionHit(boolean moveForward) {
+		// New target is next node in path
+		target = (Node) path.pop();
+
+		// Get relative heading from current location and global heading
+		Compass heading = facing.getHeadingFrom(location.getCoord(), target.getCoord());
+		// Add relative heading to global heading
+		facing = facing.add(heading);
+
+		location = target;
+
+		// Turn towards new target node if it isn't straight ahead
+		int degrees = heading.toDegrees();
+		if (degrees != 0) {
+			if (moveForward)
+				pilot.travel(4);
+			pilot.rotate(degrees);
+		}
 	}
 
-	@Override
-	public void onIntersectionDepart() {
-	}
-
+	// Sets a boolean for if each sensor is on the line or not
 	public void lineChanged(BlackLineSensor sensor, boolean onLine, int lightValue) {
-		if (sensor == lsRight)
-			synchronized (steerLeftThread) {
-				steerLeftThread.notify();
-			}
+		if (sensor == lsLeft)
+			leftOnline = onLine;
 		else
-			synchronized (steerRightThread) {
-				steerRightThread.notify();
-			}
+			rightOnline = onLine;
 	}
 }
---
+++ src/rp/Exercise/Ex3/Part2/Ex3P2Broken.java
@@ -0,0 +1,145 @@
+package rp.Exercise.Ex3.Part2;
+
+import rp.GeoffBot;
+import rp.Listener.LineListener;
+import rp.Sensor.BlackLineSensor;
+import rp.Util.RunSystem;
+
+import lejos.robotics.navigation.DifferentialPilot;
+
+import java.util.Queue;
+
+@Deprecated
+public class Ex3P2Broken extends RunSystem implements LineListener {
+	private final DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
+	private BlackLineSensor lsLeft, lsRight;
+
+	private Queue<Node> path;
+	private Node location, target;
+	private Compass facing;
+	private Thread steerLeftThread, steerRightThread;
+
+	private boolean isTravelling = false;
+
+	public Ex3P2Broken(Queue<Node> path, Node location, Compass facing) {
+		// If the path is empty then we can't do anything
+		if (path.empty())
+			return;
+
+		this.path = path;
+		this.facing = facing;
+		this.location = location;
+
+		lsLeft = new BlackLineSensor(GeoffBot.getLightSensorLeftPort(), true, 40).addChangeListener(this);
+		lsRight = new BlackLineSensor(GeoffBot.getLightSensorRightPort(), true, 40).addChangeListener(this);
+
+		GeoffBot.calibrateLeftLS(lsLeft);
+		GeoffBot.calibrateRightLS(lsRight);
+
+		// REMOVED
+		// Detect intersections
+		// new IntersectionSensor(lsLeft, lsRight, true).addChangeListener(this);
+
+		steerLeftThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (isRunning) {
+					try {
+						wait(0);
+						Thread.sleep(250);
+					}
+					catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					if (isTravelling)
+						pilot.steer(-60);
+					else
+						pilot.forward();
+				}
+			}
+		});
+		steerRightThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (isRunning) {
+					try {
+						wait(0);
+						Thread.sleep(250);
+					}
+					catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					if (isTravelling)
+						pilot.steer(60);
+					else
+						pilot.forward();
+				}
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+
+		Queue<Node> path = new Queue<>();
+		path.addElement(new Node(1, 0));
+		path.addElement(new Node(1, 1));
+		path.addElement(new Node(0, 1));
+		path.addElement(new Node(0, 0));
+		path.addElement(new Node(1, 0));
+		path.addElement(new Node(1, 1));
+		path.addElement(new Node(0, 1));
+		path.addElement(new Node(0, 0));
+
+		Ex3P2Broken program = new Ex3P2Broken(path, new Node(0, 0), Compass.UP);
+		program.run();
+	}
+
+	@Override
+	public synchronized void run() {
+		while (!path.empty() && isRunning) {
+			target = (Node) path.pop();
+
+			Compass heading = facing.getHeadingFrom(location.getCoord(), target.getCoord());
+			facing = facing.add(heading);
+
+			pilot.rotate(heading.toDegrees()); // Rotate to face target node if not already
+
+			// Drive to 'location'
+			isTravelling = true;
+			pilot.forward();
+
+			try { // the line and adjust angle while
+				this.wait(0); // Wait for intersection to be reached
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			pilot.travel(4); // Travel to align wheels with the intersection
+			isTravelling = false;
+
+			location = target;  // We are now at the target location
+		}
+	}
+
+	// @Override
+	public synchronized void onIntersectionArrive() {
+		if (isTravelling)
+			notify(); // Wake up loop to continue on path
+	}
+
+	// @Override
+	public void onIntersectionDepart() {
+	}
+
+	public void lineChanged(BlackLineSensor sensor, boolean onLine, int lightValue) {
+		if (sensor == lsRight)
+			synchronized (steerLeftThread) {
+				steerLeftThread.notify();
+			}
+		else
+			synchronized (steerRightThread) {
+				steerRightThread.notify();
+			}
+	}
+}
