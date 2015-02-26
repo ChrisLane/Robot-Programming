1fe887c165bf37b56dfabec82c054fd820c1f32d
ChrisLane <ChrisLane@users.noreply.github.com>
2015-02-26 12:53:51 +0000

    Cleanup - We need more comments here maybe

--- src/rp/Exercise/Ex3/Part3/Ex3P3.java
+++ src/rp/Exercise/Ex3/Part3/Ex3P3.java
@@ -13,12 +13,12 @@ public class Ex3P3 extends RunSystem {
 	private final DifferentialPilot pilot = GeoffBot.getDifferentialPilot();
 	private final NXTCam cam;
 
-	private double leftT = 136; // right and left thresholds before turning either side
-	private double rightT = 50;	//range of x is (15 - 171) therefore middle is 93 + 43 on each side
+	private final double leftT = 136; // right and left thresholds before turning either side
+	private final double rightT = 50;    //range of x is (15 - 171) therefore middle is 93 + 43 on each side
 	private double radius;
-	private double constant = 0.7;
+	private final double constant = 0.7;
 	private double speed = 10;
-	
+
 
 	public Ex3P3() {
 		this.cam = new NXTCam(GeoffBot.getCameraPort());
@@ -27,43 +27,44 @@ public class Ex3P3 extends RunSystem {
 		this.cam.enableTracking(true);
 	}
 
+	public static void main(String[] args) {
+		Ex3P3 program = new Ex3P3();
+		program.run();
+	}
+
 	@Override
 	public void run() {
 
 		while (this.isRunning) {
-			Rectangle2D rec = this.cam.getRectangle(0);	
-			double setPoint = 40;	//this is the width of the rectangle containing the largest tracked object
+			Rectangle2D rec = this.cam.getRectangle(0);
+
+			double setPoint = 40;    //this is the width of the rectangle containing the largest tracked object
 			double width = rec.getWidth();
-			double error = (width <= setPoint ? setPoint - width :  -(width - setPoint));
+			double error = (width <= setPoint ? setPoint - width : -(width - setPoint));
 			speed += (0.001 * error);
+
 			pilot.setTravelSpeed(speed);
 			pilot.forward();
-			
-			if(width >= 55 || width <= 20){  		//if too close or lost target stop and reset speed to default
+
+			if (width >= 55 || width <= 20) {        //if too close or lost target stop and reset speed to default
 				pilot.stop();
 				pilot.setTravelSpeed(10);
-				if (width >= 65){			//if overly close reverses away
+
+				if (width >= 65) {            //if overly close reverses away
 					pilot.backward();
 				}
 			}
-			
+
 			if (rec.getCenterX() < this.rightT) {
 				this.radius = constant * (this.rightT - rec.getCenterX());
 				this.pilot.arcForward(-this.radius);
 				Delay.msDelay(30);
-			}
-			else if (rec.getCenterX() > this.leftT) {
+			} else if (rec.getCenterX() > this.leftT) {
 				this.radius = constant * (rec.getCenterX() - this.leftT);
 				this.pilot.arcForward(this.radius);
 				Delay.msDelay(30);
 			}
-			System.out.println(rec.getWidth());
 			Delay.msDelay(30);
 		}
-	}
-
-	public static void main(String[] args) {
-		Ex3P3 program = new Ex3P3();
-		program.run();
 	}
 }
