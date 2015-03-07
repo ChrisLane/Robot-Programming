6188208e6fc2d65bdb2b6ea73ca5322c87606504
frebib <frebib@gmail.com>
2015-03-07 03:24:45 +0000

    Now checks for wall intersections too. Only one issue left with it seeing points on the top row as valid

--- src/rp/Exercise/Ex4/mapping/GridMap.java
+++ src/rp/Exercise/Ex4/mapping/GridMap.java
@@ -3,6 +3,7 @@ package rp.Exercise.Ex4.mapping;
 import rp.robotics.mapping.IGridMap;
 import rp.robotics.mapping.RPLineMap;
 
+import lejos.geom.Line;
 import lejos.geom.Point;
 import lejos.robotics.navigation.Pose;
 
@@ -33,11 +34,18 @@ public class GridMap implements IGridMap {
 	public int getYSize() {
 		return ySize;
 	}
+	public float getGridX(int x) {
+		return xStart + x * cellSize;
+	}
+	public float getGridY(int y) {
+		return yStart + y * cellSize;
+	}
+
 	public float getWidth() {
-		return xStart + xSize * cellSize;
+		return getGridX(xSize);
 	}
 	public float getHeight() {
-		return yStart + ySize * cellSize;
+		return getGridY(ySize);
 	}
 
 	@Override
@@ -53,7 +61,7 @@ public class GridMap implements IGridMap {
 
 	@Override
 	public Point getCoordinatesOfGridPosition(int x, int y) {
-		return new Point(xStart + x * cellSize, yStart + y * cellSize);
+		return new Point(getGridX(x), getGridY(y));
 	}
 	@Override
 	public boolean isValidTransition(int x1, int y1, int x2, int y2) {
@@ -61,6 +69,12 @@ public class GridMap implements IGridMap {
 		// TODO: Check for wall intersections between two points
 		if (isObstructed(x1, y1) || isObstructed(x2, y2) || !isValidGridPosition(x1, y1) || !isValidGridPosition(x2, y2))
 			return false;
+
+		Line line = new Line(getGridX(x1), getGridY(y1), getGridX(x2), getGridY(y2));
+		for (Line l : lineMap.getLines())
+			if (line.intersectsLine(l))
+				return false;
+
 		return true;
 	}
 	@Override
