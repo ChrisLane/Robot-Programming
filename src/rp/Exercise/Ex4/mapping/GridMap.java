a3189aacc113195acb7e9919e0497e0170947b5e
frebib <frebib@gmail.com>
2015-03-07 02:43:22 +0000

    Fixed a few bugs and removed A* as it doesn't work, the nodes don't have successors

--- src/rp/Exercise/Ex4/mapping/GridMap.java
+++ src/rp/Exercise/Ex4/mapping/GridMap.java
@@ -37,12 +37,13 @@ public class GridMap implements IGridMap {
 
 	@Override
 	public boolean isValidGridPosition(int x, int y) {
-		return x > 0 && y > 0 && x <= xSize && y <= ySize;
+		Point coords = getCoordinatesOfGridPosition(x, y);
+		return coords.x > 0 && coords.y > 0 && coords.x <= xSize && coords.y <= ySize;
 	}
 
 	@Override
 	public boolean isObstructed(int x, int y) {
-		return lineMap.inside(new Point(x, y));
+		return !lineMap.inside(new Point(x, y));
 	}
 
 	@Override
@@ -55,10 +56,7 @@ public class GridMap implements IGridMap {
 		Point pos1 = new Point(x1, y1);
 		Point pos2 = new Point(x2, y2);
 
-		// TODO: Discuss if this should be between two points more than 1 distance from each other#
-		// SearchFunction<Point> euclidean = (a, b) -> Math.abs(a.contents.x - b.contents.x) + Math.abs(a.contents.y - b.contents.y);
-		// SearchFunction<Point> manhattan = (a, b) -> (float) Math.sqrt(Math.pow(a.contents.x - b.contents.x, 2) + Math.pow(a.contents.y - b.contents.y, 2));
-		// return !AStar.findPathFrom(new Node<Point>(pos1), new Node<Point>(pos2), manhattan, euclidean).isNothing();
+		// TODO: Discuss if this should be between two points more than 1 distance from each other
 		return lineMap.inside(pos1) && lineMap.inside(pos2);
 	}
 	@Override
