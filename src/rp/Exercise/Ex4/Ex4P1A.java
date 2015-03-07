eebf62098ea70774dc72c9e2ac861598d17085c3
frebib <frebib@gmail.com>
2015-03-07 02:54:51 +0000

    Updated frame size

--- src/rp/Exercise/Ex4/Ex4P1A.java
+++ src/rp/Exercise/Ex4/Ex4P1A.java
@@ -54,8 +54,8 @@ public class Ex4P1A {
 		GridMapVisualisation mapVis = new GridMapVisualisation(gridMap, lineMap, 2);
 
 		frame.add(mapVis);
-		frame.pack();
-		frame.setSize(800, 600);
+		frame.setSize(700, 600);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 
