a069f12fa5c60e65b37e0f8cd82f3c7f0622f0b1
frebib <frebib@gmail.com>
2015-03-09 15:28:05 +0000

    PathFollower general fixes

--- src/rp/Exercise/Ex4/mapping/PathFollower.java
+++ src/rp/Exercise/Ex4/mapping/PathFollower.java
@@ -1,37 +1,37 @@
 package rp.Exercise.Ex4.mapping;
 
-import search.Node;
-
 import rp.GeoffBot;
 import rp.Listener.LineListener;
 import rp.Sensor.BlackLineSensor;
 import rp.Util.RunSystem;
+import search.Node;
 
 import lejos.geom.Point;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.Pose;
 
-import java.util.Queue;
+import java.util.List;
 
 public class PathFollower extends RunSystem implements LineListener {
 	private Thread followerThread;
 
 	private final DifferentialPilot pilot;
-
-	private Queue<Node<Point>> path;
-	private Pose location;
-	private Point target;
-	private boolean leftOnLine, rightOnLine, onIntersection;
 	private BlackLineSensor lsLeft, lsRight;
 
-	public PathFollower(DifferentialPilot pilot, Queue<Node<Point>> path, Pose location) {
+	private List<Node<Point>> path;
+	private Pose pose;
+	private Point target;
+	private boolean leftOnLine, rightOnLine, onIntersection;
+	private int pathCount = 0;
+
+	public PathFollower(DifferentialPilot pilot, List<Node<Point>> path, Pose location) {
 		followerThread = new Thread(this);
 
 		this.pilot = pilot;
 		this.path = path;
 		// TODO: Should use OdometryPoseProvider(pilot).getPose() instead.
 		// Need to fix conflict with Node points to actual grid locations in centimetres
-		this.location = location;
+		pose = location;
 		leftOnLine = true;
 		rightOnLine = true;
 
@@ -52,16 +52,17 @@ public class PathFollower extends RunSystem implements LineListener {
 
 	@Override
 	public void run() {
-		if (path.empty())
+		if (path.isEmpty())
 			return;
 
 		intersectionHit(false);
 		while (isRunning) {
 			if (leftOnLine && rightOnLine && !onIntersection) {
 				onIntersection = true;
-				if (path.empty())
+				if (path.isEmpty())
 					return;
 
+				pose.setLocation(target);
 				intersectionHit(true);
 			}
 			else if (!leftOnLine && !rightOnLine)
@@ -76,17 +77,15 @@ public class PathFollower extends RunSystem implements LineListener {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
 	public void intersectionHit(boolean moveForward) {
-		location.setLocation(target);
-		target = ((Node<Point>) path.pop()).contents;
+		target = path.get(++pathCount).contents;
 
-		float degrees = location.angleTo(target);
+		float degrees = pose.angleTo(target);
 		if ((int) degrees > -85 && degrees < 85) {
 			if (moveForward)
 				pilot.travel(4);
 			pilot.rotate(degrees);
-			location.rotateUpdate(degrees);
+			pose.rotateUpdate(degrees);
 		}
 	}
 	@Override
