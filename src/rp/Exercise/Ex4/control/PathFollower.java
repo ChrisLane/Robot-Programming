c6f235848f09e3aa06c35d6d45b659764f51b8b6
ChrisLane <ChrisLane@users.noreply.github.com>
2015-03-10 16:40:34 +0000

    Cleanup

--- src/rp/Exercise/Ex4/control/PathFollower.java
+++ src/rp/Exercise/Ex4/control/PathFollower.java
@@ -52,6 +52,7 @@ public class PathFollower extends RunSystem implements LineListener {
 	public void start() {
 		followThread.start();
 	}
+
 	public void stop() throws InterruptedException {
 		isRunning = false;
 		followThread.join();
@@ -61,36 +62,47 @@ public class PathFollower extends RunSystem implements LineListener {
 	public void run() {
 		intersectionHit(false);
 		while (isRunning) {
+			// Robot is now on an intersection, adjust robot to next position
 			if (leftOnLine && rightOnLine && !onIntersection && !reversing) {
 				onIntersection = true;
-				if (path.size() <= pathCount)
+				if (path.size() <= pathCount || reversing)
 					return;
 
 				intersectionHit(true);
 			}
+
+			// Robot is no longer on an intersection
 			else if (!leftOnLine && !rightOnLine)
 				onIntersection = false;
 
-			if (leftOnLine)
+			// Adjust robot to stay on the line
+			if (leftOnLine) {
 				if (!reversing)
 					pilot.arcForward(-40);
 				else
 					pilot.arcBackward(-40);
-			else if (rightOnLine)
+			} else if (rightOnLine) {
 				if (!reversing)
 					pilot.arcForward(40);
 				else
 					pilot.arcBackward(40);
+			}
+
+			// Reversed to an intersection, create and use a new path
 			else if (onIntersection && reversing) {
 				System.out.println("Reached an intersection while reversing");
 				reversing = false;
 				// TODO: Create a method to use here that can be passed a blocked coordinate and follow a new path with that data
 			}
-			else if (rangeFinder.getRange() < 5) {
+
+			// Reverse if we meet an obstacle
+			else if (rangeFinder.getRange() < 10) {
 				System.out.println("OH FUCK, A WALL!");
 				reversing = true;
 				pilot.backward();
 			}
+
+			// Default: travel forwards
 			else if (!reversing)
 				pilot.forward();
 		}
