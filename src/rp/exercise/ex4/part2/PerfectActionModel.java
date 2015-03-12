c56a1efc47135018ff98ea1d2ad05872d7d1af9f
frebib <frebib@gmail.com>
2015-03-12 01:13:31 +0000

    I'm pretty certain this does the same thing in a fraction of the code...

--- src/rp/exercise/ex4/part2/PerfectActionModel.java
+++ src/rp/exercise/ex4/part2/PerfectActionModel.java
@@ -3,6 +3,7 @@ package rp.exercise.ex4.part2;
 import rp.robotics.localisation.ActionModel;
 import rp.robotics.localisation.GridPositionDistribution;
 import rp.robotics.mapping.Heading;
+import search.Coordinate;
 
 /**
  * Example structure for an action model that should move the probabilities 1 cell in the requested direction. In the case where the move would take the robot into an obstacle or off the map, this model assumes the robot stayed in one place. This is the same as the model presented in Robot
@@ -13,139 +14,40 @@ import rp.robotics.mapping.Heading;
 public class PerfectActionModel implements ActionModel {
 
 	@Override
-	public GridPositionDistribution updateAfterMove(GridPositionDistribution _from, Heading _heading) {
-
-		// Create the new distribution that will result from applying the action
-		// model
-
-		GridPositionDistribution to = new GridPositionDistribution(_from);
-
-		// Move the probability in the correct direction for the action
-		if (_heading == Heading.RIGHT)
-			movePlusX(_from, to);
-		else if (_heading == Heading.UP)
-			movePlusY(_from, to);
-		else if (_heading == Heading.LEFT)
-			moveMinusX(_from, to);
-		else if (_heading == Heading.DOWN)
-			moveMinusY(_from, to);
-
-		return to;
-	}
-
-	/**
-	 * Move probabilities from _from one cell in the plus x direction into _to
-	 *
-	 * @param _from
-	 * @param _to
-	 */
-	private void movePlusX(GridPositionDistribution _from, GridPositionDistribution _to) {
+	public GridPositionDistribution updateAfterMove(GridPositionDistribution from, Heading heading) {
+		// Create the new distribution that will result from applying the action model
+		GridPositionDistribution to = new GridPositionDistribution(from);
 
 		// iterate through points updating as appropriate
-		for (int y = 0; y < _to.getGridHeight(); y++)
-			for (int x = 0; x < _to.getGridWidth(); x++)
+		for (int y = 0; y < to.getGridHeight(); y++)
+			for (int x = 0; x < to.getGridWidth(); x++)
 				// make sure to respect obstructed grid points
-				if (!_to.isObstructed(x, y)) {
+				if (!to.isObstructed(x, y)) {
 
 					// the action model should work out all of the different
-					// ways (x,y) in the _to grid could've been reached based on
-					// the _from grid and the move taken (in this case
-					// HEADING.PLUS_X)
+					// ways (x,y) in the to grid could've been reached based on
+					// the from grid and the move taken (in this case
+					// HEADING.PLUSX)
 
-					// for example if the only way to have got to _to (x,y) was
-					// from _from (x-1, y) (i.e. there was a PLUS_X move from
-					// (x-1, y) then you write the value from _from (x-1, y) to
-					// the _to (x, y) value
+					// for example if the only way to have got to to (x,y) was
+					// from from (x-1, y) (i.e. there was a PLUSX move from
+					// (x-1, y) then you write the value from from (x-1, y) to
+					// the to (x, y) value
 
 					// The below code does now translate the value
 
 					// position before move
-					int fromX = x - 1;
-					int fromY = y;
-					float fromProb = _from.getProbability(fromX, fromY);
+					Coordinate hd = heading.toCoordinate();
+					float fromProb = from.getProbability(x - hd.x, y - hd.y);
 
 					// position after move
 					int toX = x;
 					int toY = y;
 
 					// set probability for position after move
-					_to.setProbability(toX, toY, fromProb);
-
+					to.setProbability(toX, toY, fromProb);
 				}
-	}
 
-	private void movePlusY(GridPositionDistribution _from, GridPositionDistribution _to) {
-
-		// iterate through points updating as appropriate
-		for (int y = 0; y < _to.getGridHeight(); y++)
-			for (int x = 0; x < _to.getGridWidth(); x++)
-				// make sure to respect obstructed grid points
-				if (!_to.isObstructed(x, y)) {
-
-					// The below code does now translate the value
-
-					// position before move
-					int fromX = x;
-					int fromY = y - 1;
-					float fromProb = _from.getProbability(fromX, fromY);
-
-					// position after move
-					int toX = x;
-					int toY = y;
-
-					// set probability for position after move
-					_to.setProbability(toX, toY, fromProb);
-
-				}
-	}
-
-	private void moveMinusX(GridPositionDistribution _from, GridPositionDistribution _to) {
-
-		// iterate through points updating as appropriate
-		for (int y = 0; y < _to.getGridHeight(); y++)
-			for (int x = 0; x < _to.getGridWidth(); x++)
-				// make sure to respect obstructed grid points
-				if (!_to.isObstructed(x, y)) {
-
-					// The below code does now translate the value
-
-					// position before move
-					int fromX = x + 1;
-					int fromY = y;
-					float fromProb = _from.getProbability(fromX, fromY);
-
-					// position after move
-					int toX = x;
-					int toY = y;
-
-					// set probability for position after move
-					_to.setProbability(toX, toY, fromProb);
-
-				}
-	}
-
-	private void moveMinusY(GridPositionDistribution _from, GridPositionDistribution _to) {
-
-		// iterate through points updating as appropriate
-		for (int y = 0; y < _to.getGridHeight(); y++)
-			for (int x = 0; x < _to.getGridWidth(); x++)
-				// make sure to respect obstructed grid points
-				if (!_to.isObstructed(x, y)) {
-
-					// The below code does now translate the value
-
-					// position before move
-					int fromX = x;
-					int fromY = y + 1;
-					float fromProb = _from.getProbability(fromX, fromY);
-
-					// position after move
-					int toX = x;
-					int toY = y;
-
-					// set probability for position after move
-					_to.setProbability(toX, toY, fromProb);
-
-				}
+		return to;
 	}
 }
