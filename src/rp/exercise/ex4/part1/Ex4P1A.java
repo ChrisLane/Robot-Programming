92e31d04634eff54238a98665bb24743c5d503d1
frebib <frebib@gmail.com>
2015-03-13 05:54:40 +0000

    Code re-order in Ex4P1A

--- src/rp/exercise/ex4/part1/Ex4P1A.java
+++ src/rp/exercise/ex4/part1/Ex4P1A.java
@@ -15,26 +15,30 @@ import java.util.List;
 import javax.swing.JFrame;
 
 public class Ex4P1A {
+	private JFrame frame;
+
+	private GridMapVisualisation mapVis;
+	private RPLineMap lineMap;
+	private GridMap gridMap;
+
 	public void run() {
-		JFrame frame = new JFrame("Map Viewer");
-		frame.addWindowListener(new KillMeNow());
-		RPLineMap lineMap = MapUtils.create2015Map1();
+		lineMap = MapUtils.create2015Map1();
+		gridMap = new GridMap(12, 8, 15, 15, 30, lineMap);
+		mapVis = new GridMapVisualisation(gridMap, lineMap, 2, true);
 
-		GridMap gridMap = new GridMap(12, 8, 15, 15, 30, lineMap);
-
-		// view the map with 2 pixels as 1 cm
-		GridMapVisualisation mapVis = new GridMapVisualisation(gridMap, lineMap, 2, true);
-		List<Node<Coordinate>> path = AStar.findPathFrom(gridMap.getNodeAt(0, 0), gridMap.getNodeAt(11, 7), SearchFunction.euclidean, SearchFunction.manhattan);
+		List<Node<Coordinate>> path = AStar.findPathFrom(gridMap.getNodeAt(0, 0), gridMap.getNodeAt(11, 7), SearchFunction.euclidean, SearchFunction.manhattan, null);
 		System.out.println(path);
-		// List<Node<Coordinate>> path = new ArrayList<Node<Coordinate>>();
 
 		mapVis.setPath(path);
 
+		frame = new JFrame("Map Viewer");
+		frame.addWindowListener(new KillMeNow());
 		frame.add(mapVis);
 		frame.setSize(820, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
+
 	public static void main(String[] args) {
 		Ex4P1A demo = new Ex4P1A();
 		demo.run();
