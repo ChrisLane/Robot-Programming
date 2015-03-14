f14949f60f843f1a547d55f2751fc3d289bd3563
frebib <frebib@gmail.com>
2015-03-14 19:15:49 +0000

    Paths are sent and reconstructed. Not tested yet!

---
+++ src/rp/util/remote/packet/CoordPacket.java
@@ -0,0 +1,31 @@
+package rp.util.remote.packet;
+
+import search.Coordinate;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class CoordPacket extends RobotPacket<Coordinate> {
+	public static final byte ID = 17;
+
+	public CoordPacket(Coordinate coord) {
+		super(coord);
+	}
+	public CoordPacket(DataInputStream is) throws IOException {
+		super(is);
+	}
+
+	@Override
+	public void write(DataOutputStream os) throws IOException {
+		os.write(ID);
+		os.write(data.x);
+		os.write(data.y);
+	}
+
+	@Override
+	protected Coordinate read(DataInputStream is) throws IOException {
+		return new Coordinate(is.readByte(), is.readByte());
+	}
+
+}
