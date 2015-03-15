64136e8228be1a0b3b1fc86a71cd95c686615e4c
frebib <frebib@gmail.com>
2015-03-15 01:31:32 +0000

    NOT TESTED YET, MAY BE BROKEN: Code refactoring & cleanup.

--- src/rp/exercise/ex4/control/PathFollower.java
+++ src/rp/exercise/ex4/control/PathFollower.java
@@ -44,7 +44,11 @@ public class PathFollower extends RunSystem implements LineListener {
 		this.pilot = pilot;
 		this.path = path;
 		this.facing = facing;
+
+		pathCount = 0;
 		this.location = location;
+		target = path.get(pathCount++);
+
 		leftOnLine = true;
 		rightOnLine = true;
 
@@ -75,11 +79,16 @@ public class PathFollower extends RunSystem implements LineListener {
 
 	@Override
 	public void run() {
-		intersectionHit(false);
+		turnToTarget();
 
 		while (isRunning) {
 			// Update Pose remotely
-			lc.send(new PosePacket(poseProv.getPose()));
+			lc.send(new PosePacket(pose));
+
+			// Get range reading from US sensor & send it to the viewer
+			float range = rangeFinder.getRange();
+			lc.send(new UltrasonicDistancePacket(range));
+
 			// Robot is now on an intersection, adjust robot to next position
 			if (leftOnLine && rightOnLine && !onIntersection) {
 				onIntersection = true;
@@ -111,39 +120,35 @@ public class PathFollower extends RunSystem implements LineListener {
 			// Reversed to an intersection, create and use a new path
 			if (onIntersection && reversing)
 				reversing = false;
-			// TODO: We've reached an unexpected obstacle, re-localise the robot!
-			else {
-				float range = rangeFinder.getRange();
-				lc.send(new UltrasonicDistancePacket(range));
-				if (range < 10 && !reversing) {
-					reversing = true;
-					pilot.backward();
-				}
-				// Default: travel forwards
-				else if (!reversing)
-					pilot.forward();
+			else if (range < 10 && !reversing) {
+				reversing = true;
+				pilot.backward();
 			}
+			// Default: travel forwards
+			else if (!reversing)
+				pilot.forward();
 
 		}
 	}
 	public void intersectionHit(boolean moveForward) {
+		location = target; // TODO: Should this be here instead?
 		target = path.get(pathCount++);
 
-		Heading heading = facing.getHeadingFrom(location.payload, target.payload);
-		facing = facing.add(heading);
+		turnToTarget();
+		if (moveForward)
+			pilot.travel(2.8);
 
-		location = target;
+		// TODO: WHY DOES THIS WORK? IT SHOULDN'T BE HERE!
+		// location = target;
 
-		int degrees = heading.toDegrees();
-		if (degrees != 0) {
-			if (moveForward)
-				pilot.travel(2.8);
-			pilot.rotate(degrees);
-			// pose.setHeading(-facing.toDegrees() - degrees);
-		}
-		pose = poseProv.getPose();
 		pose.setLocation(target.payload.y * 30 + 15, target.payload.x * 30 + 15);
-		poseProv.setPose(pose);
+	}
+	public void turnToTarget() {
+		Heading heading = facing.getHeadingFrom(location.payload, target.payload);
+		if (heading != Heading.UP)
+			pilot.rotate(heading.toDegrees());
+		facing = facing.add(heading);
+		pose.setHeading(facing.toDegrees());
 	}
 
 	@Override
