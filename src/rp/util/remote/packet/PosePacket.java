7f823c7305f3473616e76683bec041ee6ceba4f1
frebib <frebib@gmail.com>
2015-03-17 11:22:08 +0000

    Added robotID to PosoPacket

--- src/rp/util/remote/packet/PosePacket.java
+++ src/rp/util/remote/packet/PosePacket.java
@@ -9,8 +9,15 @@ import java.io.IOException;
 public class PosePacket extends RobotPacket<Pose> {
 	public static final byte ID = 10;
 
+	public byte robotID;
+
 	public PosePacket(Pose pose) {
 		super(pose);
+		robotID = 0;
+	}
+	public PosePacket(Pose pose, int robotID) {
+		super(pose);
+		this.robotID = (byte) robotID;
 	}
 	public PosePacket(DataInputStream is) throws IOException {
 		super(is);
@@ -18,11 +25,13 @@ public class PosePacket extends RobotPacket<Pose> {
 
 	@Override
 	public Pose read(DataInputStream is) throws IOException {
+		robotID = is.readByte();
 		return new Pose(is.readFloat(), is.readFloat(), is.readFloat());
 	}
 	@Override
 	public void write(DataOutputStream os) throws IOException {
 		os.writeByte(ID);
+		os.writeByte(robotID);
 		os.writeFloat(data.getX());
 		os.writeFloat(data.getY());
 		os.writeFloat(data.getHeading());
