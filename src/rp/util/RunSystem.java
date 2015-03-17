a5c3fd9ff9bd3d4d1e20f1b02b558934c5fe7237
frebib <frebib@gmail.com>
2015-03-17 11:17:19 +0000

    Made RunSystem implement ButtonListener so it can be overridden

--- src/rp/util/RunSystem.java
+++ src/rp/util/RunSystem.java
@@ -3,20 +3,19 @@ package rp.util;
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
 
-public abstract class RunSystem implements Runnable {
+public abstract class RunSystem implements Runnable, ButtonListener {
 	protected boolean isRunning = true;
 
 	public RunSystem() {
-		Button.ESCAPE.addButtonListener(new ButtonListener() {
-			@Override
-			public void buttonReleased(Button _b) {
-			}
+		Button.ESCAPE.addButtonListener(this);
+	}
+	@Override
+	public void buttonReleased(Button _b) {
+	}
 
-			@Override
-			public void buttonPressed(Button _b) {
-				isRunning = false;
-				System.exit(0);
-			}
-		});
+	@Override
+	public void buttonPressed(Button _b) {
+		isRunning = false;
+		System.exit(0);
 	}
 }
