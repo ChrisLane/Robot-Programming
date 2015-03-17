ed3b163e5a25b6997a8406abf5be2d1c2b46d0fc
frebib <frebib@gmail.com>
2015-03-17 11:21:39 +0000

    Remove successors from PathPacket. They're not needed

--- src/rp/util/remote/packet/PathPacket.java
+++ src/rp/util/remote/packet/PathPacket.java
@@ -7,11 +7,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@SuppressWarnings("deprecation")
 public class PathPacket extends RobotPacket<List<Node<Coordinate>>> {
 	public static final byte ID = 15;
 	public static final byte NodeID = 16;
@@ -25,47 +22,21 @@ public class PathPacket extends RobotPacket<List<Node<Coordinate>>> {
 
 	@Override
 	public void write(DataOutputStream os) throws IOException {
-		os.write(ID);											// path id
-
-		for (Node<Coordinate> n : data) {
-			os.write(NodeID);									// node id
+		os.writeByte(ID);										// path id
+		os.writeShort(data.size());								// node count
+		for (Node<Coordinate> n : data)
 			new CoordPacket(n.payload).write(os);				// node payload *the coordinate*
-
-			os.write((byte) n.getSuccessors().size());			// node successor count
-			for (Node<Coordinate> succ : n.getSuccessors())
-				os.writeShort(succ.payload.hashCode());			// all successor hashcodes
-		}
 	}
 	@Override
 	protected List<Node<Coordinate>> read(DataInputStream is) throws IOException {
 		short size = is.readShort();							// node count
 
-		List<short[]> allSuccessors = new ArrayList<short[]>(size);
-		Map<Short, Node<Coordinate>> nodes = new HashMap<Short, Node<Coordinate>>(size);
-
-		for (int i = 0; i < size; i++) {
-			if (is.readByte() != NodeID)							// node id
-				throw new IOException("Data received is not Path format");
-
-			Coordinate c = new CoordPacket(is).getData();			// node payload *the coordinate*
-			Node<Coordinate> n = new Node<Coordinate>(c);
-			short nodeHash = (short) c.hashCode();
-			nodes.put(nodeHash, n);
-
-			short[] nSucs = new short[is.readByte() + 1];			// node successor count
-			nSucs[0] = nodeHash;
-			for (int j = 1; j < nSucs.length; j++)
-				nSucs[j] = is.readShort();							// all successor hashcodes
-			allSuccessors.add(nSucs);
-		}
-
 		data = new ArrayList<Node<Coordinate>>(size);
-		for (int i = 0; i < size; i++) {							// Reconstruct Nodes
-			short[] nodeSuccs = allSuccessors.get(i);				// with successors
-			Node<Coordinate> node = nodes.get(nodeSuccs[0]);
-			for (short successorHash : nodeSuccs)
-				node.addSuccessor(nodes.get(successorHash));		// Doesn't matter if Node is null here
-			data.add(node);
+		for (int i = 0; i < size; i++) {
+			if (is.readByte() != CoordPacket.ID)					// coordinate id
+				throw new IOException("Data received is not Coordinate format");
+			Coordinate c = new CoordPacket(is).getData();			// node payload *the coordinate*
+			data.add(new Node<Coordinate>(c));
 		}
 		return data;
 	}
