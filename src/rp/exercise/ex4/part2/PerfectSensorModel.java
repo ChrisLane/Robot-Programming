90699c2d060769844299613861971046971398f9
ChrisLane <ChrisLane@users.noreply.github.com>
2015-03-23 18:40:04 +0000

    Add PerfectSensorModel

--- src/rp/exercise/ex4/part2/PerfectSensorModel.java
+++ src/rp/exercise/ex4/part2/PerfectSensorModel.java
@@ -1,4 +1,30 @@
 package rp.exercise.ex4.part2;
 
-public class PerfectSensorModel {
+import lejos.robotics.RangeReadings;
+import rp.robotics.localisation.GridPositionDistribution;
+import rp.robotics.localisation.SensorModel;
+import rp.robotics.mapping.Heading;
+
+public class PerfectSensorModel implements SensorModel {
+    @Override
+    public GridPositionDistribution updateAfterSensing(GridPositionDistribution _dist, Heading _heading, RangeReadings _readings) {
+
+        for (int x = 0; x < _dist.getGridWidth(); x++) {
+            for (int y = 0; y < _dist.getGridHeight(); y++) {
+
+                if (_dist.isValidGridPosition(x, y)) {
+                    float obstacleDistance = _dist.getGridMap().rangeToObstacleFromGridPosition(x, y, (float) _heading.toDegrees());
+
+                    if (obstacleDistance == _readings.getRange(0))
+                        _dist.setProbability(x, y, 1);
+                    else {
+                        _dist.setProbability(x, y, 0);
+                    }
+                    _dist.normalise();
+                }
+            }
+        }
+
+        return _dist;
+    }
 }
