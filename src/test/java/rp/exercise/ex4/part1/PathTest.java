7ec705b007d7143b993803c73a6b81ff602587c1
ChrisLane <ChrisLane@users.noreply.github.com>
2015-03-26 18:07:04 +0000

    Started on the PathTest. Contains commented code

--- src/test/java/rp/exercise/ex4/part1/PathTest.java
+++ src/test/java/rp/exercise/ex4/part1/PathTest.java
@@ -1,4 +1,36 @@
 package rp.exercise.ex4.part1;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import rp.exercise.ex4.mapping.GridMap;
+import rp.robotics.mapping.MapUtils;
+import rp.robotics.mapping.RPLineMap;
+
+import java.util.Random;
+
 public class PathTest {
+	private static GridMap gridMap;
+
+	@BeforeClass
+	public void setUp() {
+		RPLineMap lineMap = MapUtils.create2015Map1();
+		gridMap = new GridMap(12, 8, 15, 15, 30, lineMap);
+	}
+
+	@DataProvider(name = "randomPositions")
+	public static Object[][] randomPositions() {
+		Object[][] nodes = new Object[10][2];
+		Random random = new Random();
+
+		for (int i = 0; i < 10; i++) {
+			int x = random.nextInt(gridMap.getXSize());
+			int y = random.nextInt(gridMap.getYSize());
+
+			if (gridMap.isValidGridPosition(x, y)) {
+				//nodes[i] = gridMap.getNodeAt(x, y);
+			}
+		}
+
+		return new Object[0][];
+	}
 }
